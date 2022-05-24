08a805ad044b82516e865dd6310b036b4b7765b6
Griotold <101307758+Griotold@users.noreply.github.com>
2022-05-24 14:39:22 +0900

    Add files via upload

--- bash/top5_search.py
+++ bash/top5_search.py
@@ -15,7 +15,11 @@ datas = []
 for tr in trs:
     name = tr.select_one('th > a').get_text()
     current_price = tr.select_one('td').get_text() 
-    change_direction = tr['class'][0]
+    change_direction = []
+    if tr['class'][0] == "up":
+        change_direction.append("▲")
+    else:
+        change_direction.append("▼")
     change_price = tr.select_one('td > span').get_text()
     datas.append([name, current_price, change_direction, change_price])
 
