819e9ba0c1dfd5d5251af2d09f4a1d73848d173c
kmk3593 <81403633+kmk3593@users.noreply.github.com>
2022-05-27 17:38:00 +0900

    Add files via upload

--- wsl/insert.py
+++ wsl/insert.py
@@ -3,8 +3,8 @@
 import psycopg2
 
 
-passwd = '5280c3caeed8c1cb512f19d6fc238a6ab642556e69c3050ddfe232c4c4372d0e'
-db = psycopg2.connect(host='ec2-3-234-131-8.compute-1.amazonaws.com', dbname='d33p984cbdpnn',user='ndurbfpbebgdrc',password= passwd,port=5432)
+passwd = ''
+db = psycopg2.connect(host='', dbname='',user='',password= passwd,port=5432)
 cur=db.cursor()
 
 # cur.execute("DROP TABLE test")
