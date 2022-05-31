865e31a332ea6497fbbb72f32d2df71b745b858a
WoobinHwang <95206741+WoobinHwang@users.noreply.github.com>
2022-05-31 16:18:10 +0900

    Update sellitem.py

--- wsl/Woobin/postgres/sellitem.py
+++ wsl/Woobin/postgres/sellitem.py
@@ -4,7 +4,7 @@ import requests
 import pandas_datareader.data as web
 
 
-passwd =                'b55d94be7d8dbef24e28a72a0dcb228fb48d1595665100e8da2cd1aafbe8bbbc'
+passwd =                ''
 db = psycopg2.connect(host='ec2-54-204-56-171.compute-1.amazonaws.com', dbname='d2p5j2up8o05rg',user='dywzgxybcyjnzu',password= passwd,port=5432)
 cur=db.cursor()
 
