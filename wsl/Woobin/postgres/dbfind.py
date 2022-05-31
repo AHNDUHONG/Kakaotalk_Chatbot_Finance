b55678c1824d2e9b136119545cbfe369d9387e56
WoobinHwang <95206741+WoobinHwang@users.noreply.github.com>
2022-05-31 16:17:03 +0900

    Update dbfind.py

--- wsl/Woobin/postgres/dbfind.py
+++ wsl/Woobin/postgres/dbfind.py
@@ -1,7 +1,7 @@
 import psycopg2
 from datetime import datetime
 
-passwd =                'b55d94be7d8dbef24e28a72a0dcb228fb48d1595665100e8da2cd1aafbe8bbbc'
+passwd =                ''
 db = psycopg2.connect(host='ec2-54-204-56-171.compute-1.amazonaws.com', dbname='d2p5j2up8o05rg',user='dywzgxybcyjnzu',password= passwd,port=5432)
 cur=db.cursor()
 date_data = "'%s'" %(datetime.today().strftime('%Y-%m-%d'))
