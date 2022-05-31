2795d16e34b1cfa64c8f9d57ec68b9b379a3db7b
WoobinHwang <95206741+WoobinHwang@users.noreply.github.com>
2022-05-31 16:17:21 +0900

    Update dbinsert.py

--- wsl/Woobin/postgres/dbinsert.py
+++ wsl/Woobin/postgres/dbinsert.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # print(datetime.today().strftime('%Y-%m-%d'))
 
 
-passwd =                'b55d94be7d8dbef24e28a72a0dcb228fb48d1595665100e8da2cd1aafbe8bbbc'
+passwd =                ''
 db = psycopg2.connect(host='ec2-54-204-56-171.compute-1.amazonaws.com', dbname='d2p5j2up8o05rg',user='dywzgxybcyjnzu',password= passwd,port=5432)
 cur=db.cursor()
 
